61240b9f898baa2d2e04a23a77461ad36a5608bb
Adrian Sinnott <omgitsadrian@gmail.com>
2018-08-20 09:47:30 -0700

    Correct 'Salmon Creek Big Bug' misspelling

--- src/App.js
+++ src/App.js
@@ -328,7 +328,7 @@ class App extends Component {
                         <div>
                           <h2>Who We Are</h2>
                           <p>Cowboy Chronic founder Forest Elie was born and raised in the “Wild West” of Southern Humboldt and introduced to farming cannabis at age 11. At age 16, Forest received a job offer from Chris Giauque, a legendary grower sometimes referred to as the Paul Bunyon of Humboldt.</p>
-                          <p>Forest worked for chris from 1998 to 2002, learning from the Salmon Creek guru and producing “Solmon Creek Big Bud” on a commercial level. Forest later went on to found Cowboy Chronic Brand in 2016.</p>
+                          <p>Forest worked for chris from 1998 to 2002, learning from the Salmon Creek guru and producing “Salmon Creek Big Bud” on a commercial level. Forest later went on to found Cowboy Chronic Brand in 2016.</p>
                           <p>Forest is also well-known for jumping, handcuffed, from a 12-foot balcony at a Southern Humboldt party, evading six officers in the process and sleeping it off in a nearby creek bed. A true <span style={{fontStyle: "Italic"}}>West Coast Bandit</span> !</p>
                           <p>With decades of experience farming top-quality cannabis, we invite you to grow and smoke with us!</p>
                         </div>
